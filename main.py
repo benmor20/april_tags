59537dad5024fd4ae3ad899632f119cf1e51db78
Maya Cranor <mcranor@olin.edu>
2023-11-05 21:25:12 -0500

    Cleaned up, commented, and refactored main file

--- main.py
+++ main.py
@@ -13,31 +13,6 @@ from angle_utils import angle_diff
 from matplotlib import pyplot as plt
 
 
-def show_tree(tree):
-    """
-    Display a graph with a graphical interface
-
-    tree: a NetworkX graph that represents all potential combinations of segments 
-    
-    """
-    # Define the layout with root at the top center
-    layout = nx.drawing.nx_agraph.graphviz_layout(tree, prog="dot")
-    
-    # Create a figure and axis
-    fig, ax = plt.subplots(figsize=(8, 6))
-    
-    # Set the y-coordinates to be negative to reverse the tree orientation
-    for node, coords in layout.items():
-        layout[node] = (coords[0], -coords[1])
-    
-    # Draw the tree with customized layout
-    nx.draw(tree, layout, with_labels=True, node_size=500, node_color="skyblue", font_size=10, ax=ax)
-    
-    # Show the plot
-    plt.title("Segment Tree")
-    plt.show()
-
-
 def camera_capture(show_image: bool = False) -> np.ndarray:
     """
     Capture an image from the laptop's webcam using OpenCV
@@ -73,7 +48,7 @@ def april_tag_detector(image_matrix: np.ndarray):
     quad_detector(segments)
 
 
-def segmentation(black_white_matrix: np.ndarray) -> np.ndarray: # Alana
+def segmentation(black_white_matrix: np.ndarray) -> np.ndarray:
     """
     Find line segments that border black and white shapes
     
@@ -98,12 +73,14 @@ def segmentation(black_white_matrix: np.ndarray) -> np.ndarray: # Alana
         segment_array[i,:] = cluster_list[i].to_segment()
     return segment_array
 
+
 def compute_gradient(black_white_matrix: np.ndarray) -> np.ndarray:
     """
     Calculate the gradient of each pixel in the image.
 
     The gradient is calculated using a 3x3 Scharr operator as a convolution
-    kernel.
+    kernel. The Scharr kernel is similar to a Sobel kernel, but more accurate
+    as a 3x3 matrix.
 
     Args:
         black_white_matrix: a [w x h] numpy array giving the black and white
@@ -113,16 +90,15 @@ def compute_gradient(black_white_matrix: np.ndarray) -> np.ndarray:
         a [w x h x 2] numpy array giving the gradient at each pixel - axes are
             x by y by (magnitude, direction). Direction is in degrees.
     """
-    # for each pixel, find gradient x, gradient y, magnitude, direction
-    # there is a sobel operator built into cv2 which might be easier to use
-    # Scharr kernel is similar to Sobel kernel, but more accurate as a 3x3 matrix
-    
+    # For each pixel, find gradient x, gradient y, magnitude, direction
     scharr_x_kernel = np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]])
     scharr_y_kernel = np.array([[-3, -10, -3], [0, 0, 0], [3, 10, 3]])
     gradient_array = np.zeros(shape = (np.shape(black_white_matrix)[0], np.shape(black_white_matrix)[1], 2))
-        # expects a float for magnitude, None type radian for direction
+    
+    # Expects a float for magnitude, None type radian for direction
     x_grads_array = signal.convolve2d(black_white_matrix, scharr_x_kernel, 'same')
-        # spits out an array of the same dimensions as the first entered array
+    
+    # Spits out an array of the same dimensions as the first entered array
     y_grads_array = signal.convolve2d(black_white_matrix, scharr_y_kernel, 'same')
     for y in range(int(np.shape(gradient_array)[0])): # goes column by column
         for x in range(int(np.shape(gradient_array[y])[0])): # goes row by row
@@ -212,10 +188,14 @@ def create_cluster_graph(mag_dir_matrix: np.ndarray) -> nx.Graph:
     Returns:
         a networkx Graph of the image to use to create clusters
     """
+    # Create a graph that contains one node per pixel
     graph = nx.Graph()
     graph.add_nodes_from(range(mag_dir_matrix.shape[0] * mag_dir_matrix.shape[1]))
+
+    # Loop through all the nodes
     row_len = mag_dir_matrix.shape[1]
     for pixel_num in graph.nodes:
+        # Define neighboring pixels
         pixel = pixel_num % row_len, pixel_num // row_len
         neighbors = [
             (pixel[0], pixel[1] + 1),
@@ -223,22 +203,30 @@ def create_cluster_graph(mag_dir_matrix: np.ndarray) -> nx.Graph:
             (pixel[0] + 1, pixel[1]),
             (pixel[0] - 1, pixel[1])
         ]
+        # Get the direction (angle) of the current pixel
         pixel_angle = mag_dir_matrix[pixel[1], pixel[0], 1]
+
+        # Loop through neighboring pixel nodes
         for neighbor in neighbors:
             if neighbor[0] >= row_len or neighbor[1] >= mag_dir_matrix.shape[0]\
                     or any(n < 0 for n in neighbor):
-                continue
+                continue # Skip neighbors that are outside the image boundaries
             neighbor_num = neighbor[1] * row_len + neighbor[0]
+
+            # Check if an edge already exists between the current pixel and the neighbor
             if graph.has_edge(pixel_num, neighbor_num):
                 continue
+
+            # Check if neighbor node exists in the graph
             if neighbor_num in graph.nodes:
+                # Calculate the difference in angles between the current pixel and the neighbor
                 neighbor_angle = mag_dir_matrix[neighbor[1], neighbor[0], 1]
                 weight = angle_diff(pixel_angle, neighbor_angle)
                 graph.add_edge(pixel_num, neighbor_num, weight=weight)
     return graph
     
     
-def quad_detector(segments: np.ndarray) -> np.ndarray:  # Anusha
+def quad_detector(segments: np.ndarray) -> np.ndarray:
     """
     Find possible quadrilaterals in the image given the list of line segments
     
@@ -250,19 +238,25 @@ def quad_detector(segments: np.ndarray) -> np.ndarray:  # Anusha
         a [q x 4 x (x, y)] numpy array containing q quads determined by corners
             listed in (x, y)
     """
+    # Build lookup tables
     dist_lookup = line_segment_distances(segments)
     cross_lookup = cross_table(segments)
+
+    # Build and search tree for quads
     tree_graph = generate_tree(segments, dist_lookup, cross_lookup)
     quads = get_quads_from_tree(tree_graph)
-    show_tree(tree_graph)
+    # show_tree(tree_graph)
     return quads
 
 
-def line_segment_distances(segments: np.ndarray) -> np.ndarray: # Anusha
+def line_segment_distances(segments: np.ndarray) -> np.ndarray:
     """
-    Creates a lookup table of distances between the end of one line segment to
-        the start of all others. Does this for all line segments found via
-        segmentation function.
+    Creates a boolean lookup table representing if two segments can be neighboring segments.
+    
+    Calculates the distance between end point of the first line segment and the
+        start point of the next segment. Compares that distance to the threshold
+        distance. The threshold is calculates from the length of the first segment
+        multiplied by 2 and adding 5.
     
     Args:
         segments: a [n x (x1, y1, x2, y2)] numpy array giving the start and end
@@ -271,35 +265,44 @@ def line_segment_distances(segments: np.ndarray) -> np.ndarray: # Anusha
     Returns:
         a [n x n] numpy array dist_lookup that returns True if the start of
         line B is within 2 times with length of line A plus five pixels. A is
-        indexed by row, B by column. Points along the diagonal return True.
+        indexed by row, B by column.
     """
+    # Find the number of segments
     nsegments = segments.shape[0]
+
+    # Create an [n x n] grid
     a, b = np.meshgrid(np.arange(nsegments), np.arange(nsegments))
+
+    # Calculate the distances between each segment in the grid
     dist_sq = (segments[a, 3] - segments[b, 1]) ** 2 + (segments[a, 2] - segments[b, 0]) ** 2
     dists = np.sqrt(dist_sq)
     lengths = np.diag(dists)
+
+    # Check if the distance is less than the threshold
     dist_lookup = dists < 2 * lengths + 5
     return dist_lookup
 
 
 def generate_tree(segments: np.ndarray, dist_lookup: np.ndarray, cross_lookup: np.ndarray) -> nx.Graph:
     """
-    Generate a tree of line segments to detect quadrilaterals from
-    
+    Generate a tree of line segments to detect quadrilaterals
 
     The root of the graph points to every possible line segment. From there,
     each child node points to every line segment whose start is "close enough"
     to the parent node's end. Close enough is defined as 2*parent length plus
-    five pixels. Repeat for four levels deep.
+    five pixels. Additionally each child node must obey the winding order of
+    the previous branches. Winding order means that each segment points in the
+    same direction. Repeat for four levels deep.
 
     Args:
         segments: a [n x (x1, y1, x2, y2)] numpy array giving the start and end
             points of each line segment, so that traveling from point 1 to
             point 2 has the light side on the right.
-        dist_lookup: a [n x n] numpy array giving the distance (in pixels)
-            between the end line A and the start of line B for all A and B. A
-            is indexed by row, B by column. Points along the diagonal give
-            the length of the line.
+        dist_lookup: a [n x n] numpy array dist_lookup that returns True if the start of
+            line B is within 2 times with length of line A plus five pixels. A is
+            indexed by row, B by column.
+        cross_lookup: a [n x n] numpy array that returns the direction of two vectors as
+            a -1, 0, or 1. 
     
     Returns:
         a NetworkX graph giving the tree of line s
@@ -310,21 +313,21 @@ def generate_tree(segments: np.ndarray, dist_lookup: np.ndarray, cross_lookup: n
     root = "Root"
     graph.add_node(root)
 
-    # level 1
+    # Level 1: add all segments as node to root
     for i in range(segments.shape[0]):
         child = (i, 1)
         graph.add_edge(root, child)
-        # level 2
+        # Level 2: add only new segments, check distances and winding order
         for j in range(segments.shape[0]):
             if j != i and dist_lookup[i][j] and cross_lookup[i][j] != 0:
                 child2 = (j, i, 2)
                 graph.add_edge(child, child2)
-                # level 3
+                # Level 3: add only new segments, check distances and winding order
                 for k in range(segments.shape[0]):
                     if k != i and k != j and dist_lookup[j][k] and cross_lookup[j][k] == cross_lookup[i][j]:
                         child3 = (k, j, i, 3)
                         graph.add_edge(child2, child3)
-                        # level 4
+                        # Level 4: add only new segments, check distances and winding order
                         for l in range(segments.shape[0]):
                             if l != i and l != k and l != j and dist_lookup[k][l] and cross_lookup[k][l] == cross_lookup[i][j]:
                                 child4 = (l, k, j , i, 4)
@@ -337,10 +340,8 @@ def get_quads_from_tree(segment_tree: nx.Graph) -> np.ndarray:
     Takes tree and conducts a depth-first search for possible quadrilaterals
         based on number of line segments.
 
-    Follows possible quads in a consistent direction clockwise or 
-    counter-clockwise based on first two line segments. Rejects quad if third
-    line segment is over pi radians away. Also rejects quad if last segment
-    does not end "close enough" to start of first  segment.
+    Finds all branches with exactly four nodes and appends them to quads. Each value
+    in the quad is the index of the segment from the segment list.
 
     Args:
         tree: a NetworkX graph that represents all potential combinations of segments
@@ -359,13 +360,14 @@ def dfs(node, path, segment_tree, quads) -> np.ndarray:
     """
     Depth first searches a tree for all paths containing four nodes.
 
-    Recursively searches through a tree using depth first search.
+    Finds all branches with exactly four nodes and appends them to quads. Each value
+    in the quad is the index of the segment from the segment list. Removes any repeat
+    quads before appending.
 
     Args:
         node: the next node that will be added to a path
         path: a list that contains the indexes for a potential quad
         segment_tree: a NetworkX graph that represents all potential combinations of segments
-
         quads: a [q x 4 x (x1, y1, x2, y2)] numpy array that each new quad
             gets added to
 
@@ -373,10 +375,12 @@ def dfs(node, path, segment_tree, quads) -> np.ndarray:
         a [q x 4 x (x1, y1, x2, y2)] numpy array where 4 x (x1, y1, x2, y2)
             determines the four line segments making up the quad
     """
+    # Check if all four segments have been found
     if len(path) == 4:
-        if path[0] == min(path):
+        if path[0] == min(path):    # Prevents quads with the same segment from existing
             quads.append(path)
         return
+    # Checks all children of a node
     for child in list(segment_tree.successors(node)):
         dfs(child, path + [child[0]], segment_tree, quads)
     return np.array(quads)
@@ -400,6 +404,7 @@ def cross_table(segments: np.ndarray)-> np.ndarray:
     num_segments = len(segments)
     cross_table = np.zeros((num_segments, num_segments))
 
+    # Loop through every value
     for i in range(len(segments)):
         for j in range(len(segments)):
             parent = np.array([segments[i][2]-segments[i][0], segments[i][3]-segments[i][1]])
@@ -409,10 +414,36 @@ def cross_table(segments: np.ndarray)-> np.ndarray:
     return cross_table
 
 
+def show_tree(tree):
+    """
+    Display a graph with a graphical interface
+
+    tree: a NetworkX graph that represents all potential combinations of segments 
+    """
+    # Define the layout with root at the top center
+    layout = nx.drawing.nx_agraph.graphviz_layout(tree, prog="dot")
+    
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(8, 6))
+    
+    # Set the y-coordinates to be negative to reverse the tree orientation
+    for node, coords in layout.items():
+        layout[node] = (coords[0], -coords[1])
+    
+    # Draw the tree with customized layout
+    nx.draw(tree, layout, with_labels=True, node_size=500, node_color="skyblue", font_size=10, ax=ax)
+    
+    # Show the plot
+    plt.title("Segment Tree")
+    plt.show()
+
+
 def main():
+    # Load in the picture
     color_matrix = cv2.imread('images/black_rectangle.png')
-    print(color_matrix.shape)
-    bw_image = cv2.cvtColor(color_matrix, cv2.COLOR_BGR2GRAY)
+
+    # Call the april tag detector
+    # quads = april_tag_detector(color_matrix)
 
     # segments = np.array([[1, 0, 437, 0],
     #                      [381, 105, 59, 105],
@@ -454,7 +485,6 @@ def main():
         plt.plot(points[:, 0], points[:, 1])
     plt.show()
 
-    # april_tag_detector(color_matrix)
     
 
 if __name__ == '__main__':
